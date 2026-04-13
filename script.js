bd4ddf06f9e2d3eca5e3dd3725bfbd609070075f
juansebastiangarcia1006-cpu <juansebastiangarcia1006@gmail.com>
2026-04-13 08:14:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+body{
+    margin:0;
+    font-family: 'Segoe UI', sans-serif;
+    background:#0f172a;
+    color:white;
+}
+
+/* HEADER */
+header{
+    background:#020617;
+    text-align:center;
+    padding:20px;
+    font-size:24px;
+    font-weight:bold;
+}
+
+/* NAV */
+nav{
+    background:#1e293b;
+    text-align:center;
+    padding:10px;
+}
+
+nav button{
+    margin:5px;
+    padding:10px 15px;
+    border:none;
+    border-radius:20px;
+    background:#38bdf8;
+    color:black;
+    font-weight:bold;
+    cursor:pointer;
+}
+
+/* SECCIONES */
+.seccion{
+    display:none;
+    padding:20px;
+    max-width:420px;
+    margin:auto;
+}
+
+/* BUSCADOR */
+.buscador{
+    width:100%;
+    padding:12px;
+    border-radius:25px;
+    border:none;
+    margin-bottom:15px;
+    font-size:16px;
+}
+
+/* TARJETAS */
+.tarjeta{
+    background:#1e293b;
+    padding:15px;
+    margin:12px 0;
+    border-radius:15px;
+    box-shadow:0 4px 10px rgba(0,0,0,0.4);
+}
+
+/* BOTONES */
+button.grande{
+    width:100%;
+    padding:12px;
+    border:none;
+    border-radius:25px;
+    background:#22c55e;
+    color:white;
+    font-weight:bold;
+    cursor:pointer;
+    margin-top:5px;
+}
+
+/* INPUTS */
+input, textarea{
+    width:95%;
+    padding:12px;
+    margin:8px 0;
+    border-radius:10px;
+    border:none;
+}
+
+/* FOOTER */
+footer{
+    background:#020617;
+    text-align:center;
+    padding:10px;
+    margin-top:20px;
+}
